e698afa2314587edcbdf3b93a2fb7d9c35d28258
Tanya Tsvirova <ta.cvirova@yandex.ru>
2023-02-27 21:32:29 +0300

    refactor: добавлены классы Popup, UserInfo, Section

---
+++ scripts/Section.js
@@ -0,0 +1,17 @@
+export class Section {
+    constructor({items, renderer}, selector) {
+        this._initialArray = items;
+        this._renderer = renderer;
+        this._container = document.querySelector(selector);
+    }
+    
+    renderItems() {
+        this._initialArray.forEach(item => {
+            this._renderer(item);
+        });
+    }
+
+    setItem(element) {
+        this._container.append(element);
+    }
+}
